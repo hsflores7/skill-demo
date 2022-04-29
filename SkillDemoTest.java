caeeb4f0347d0b86178026cc86b8b546893a3ef1
hsflores7 <floreshunter95@gmail.com>
2022-04-29 08:12:37 -0700

    Added Tester

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void addTest() {
+        assertEquals(1, SkillDemo.add(1, 2));
+    }
+
+}
